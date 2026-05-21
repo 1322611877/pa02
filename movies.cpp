2abdc08ffef516ee0375c3b81d36b330c215afe3
xiaoeni <jialuxing@ucsb.edu>
2026-05-20 22:47:33 -0700

    Add files via upload

---
+++ movies.cpp
@@ -0,0 +1,37 @@
+#include "movies.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// void addMovie(string &name, double &rating);
+// void printAllMovies();
+// vector<Movie> findByprefix(string &prefix);
+
+// map<string, double> movies;
+
+void MoviesFunc::addMovie(string &name, double rating) {
+    movies_[name] = rating;
+}
+
+void MoviesFunc::printAllSorted() {
+    for (auto &mv : movies_) {
+        cout << mv.first << ", " << mv.second << endl;
+    }
+}
+
+vector<Movie> MoviesFunc::findByprefix(string &prefix){
+    vector<Movie> result;
+    if (prefix.empty()) return result;
+    auto it = movies_.lower_bound(prefix);
+    int size = prefix.size();
+
+    while (it != movies_.end()) {
+        const string &name = it->first;
+        if (name.size() < size || name.compare(0, size, prefix) != 0)
+            break;
+        result.push_back({name,it->second});
+        ++it;
+    }
+        return result;
+    }
---
+++ movies.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <map>
+
+using namespace std;
+
+struct Movie {
+    string name;
+    double rating;
+};
+
+class MoviesFunc {
+    public:
+    //fundamental function -> add movie
+    void addMovie(string &name, double rating);
+    //print all movie in name order
+    void printAllSorted();
+    //return all movies start with given prefix
+    vector<Movie> findByprefix(string &prefix);
+    private:
+    map<string, double> movies_;
+};
